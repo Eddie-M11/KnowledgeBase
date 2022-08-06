893aaa2f22847303de43955d3616be5176133bb0
SolenIyassu <Solen.Iyassu@gmail.com>
2022-08-06 12:54:19 -0600

    completed the userRoutes

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,42 +1,55 @@
+const { response } = require("express");
 const { User } = require("../../models");
 const router = require("express").Router();
 
-router.post('/', async(req, res)=>{
-    try {
-        const loginData = await User.create(req.body)
+router.post("/", async (req, res) => {
+  try {
+    const loginData = await User.create(req.body);
 
-        req.session.save(()=>{
-            req.session.user
-        })
+    req.session.save(() => {
+      req.session.user_id = loginData.id;
+      req.session.logged_in = true;
 
-    } catch (error) {
-        
+      res.status(200).json(loginData);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post("./login", async (req, res) => {
+  try {
+    const loginData = await User.findOne({ where: { email: req.body.email } });
+    if (!loginData) {
+      res.status(400).json({
+        message: "Please check your email or password, and try again",
+      });
+      return;
     }
-
-
-}
-
-router.post('./login', async(req, res) => {
-    try {
-    
-} catch (error) {
-    
-}
-
-
-
-
-} )
-
-router.post('./logout', async(req, res)=>{
-    try {
-        
-    } catch (error) {
-        
+    const correctPassword = await loginData.checkPassword(req.body.password);
+    if (!correctPassword) {
+      res.status(400).json({
+        message: "Please check your email or password, and try again",
+      });
+      return;
     }
+    req.session.save(() => {
+      req.session.user_id = loginData.id;
+      req.session.logged_in = true;
 
+      res.json({ message: "You have successfully logged in!" });
+    });
+  } catch (error) {
+    res.status(400).json();
+  }
+});
 
-
-
-
-})
+router.post("./logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(200).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
